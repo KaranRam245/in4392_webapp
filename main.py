5ff91d45c714e6dae722793aea5787ae2b7d6161
Frenk <superdeboeren@hotmail.com>
2020-05-31 20:26:45 +0200

    Main function extended to start on different instances when args provided

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 """
 Main function to be called when starting the AWS main instance.
 """
+import aws
 import sys
 sys.path.append('./src')
-from aws.instancemanager.instancemanager import start_node_scheduler
+
+
+def main():
+    args = str(sys.argv)
+    if not len(args):
+        aws.instancemanager.instancemanager.start_instance()
+    else:
+        if args[0] == 'instance_manager':
+            aws.instancemanager.instancemanager.start_instance()
+        elif args[0] == 'node_manager':
+            aws.nodemanager.nodemanager.start_instance()
+        elif args[0] == 'worker' or args[0] == 'node_worker':
+            aws.nodeworker.nodeworker.start_instance()
+        elif args[0] == 'resource_manager':
+            aws.resourcemanager.resourcemanager.start_instance()
 
 
 # Main function to start the InstanceManager
 if __name__ == '__main__':
-    start_node_scheduler()
+    main()
