4b8bdf18d30c1d27bdabe9811609c3337e5b0650
Frenk <superdeboeren@hotmail.com>
2020-06-02 12:42:59 +0200

    Removed socket reuse

--- src/aws/utils/connection.py
+++ src/aws/utils/connection.py
@@ -28,7 +28,6 @@ class Server(socketserver.BaseRequestHandler):
 
 class Client:
     def __init__(self, host=HOST, port=PORT):
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
