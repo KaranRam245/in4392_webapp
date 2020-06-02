f36a3f9e0763af7734d915a5c3f66ea09788c5fc
Frenk <superdeboeren@hotmail.com>
2020-06-02 10:50:24 +0200

    Fixed argument issue in main

--- main.py
+++ main.py
@@ -9,23 +9,27 @@ import aws
 
 def main():
     args = str(sys.argv)
-    print(args)
-    if not len(args):
+
+    if len(args) < 2:
         print('[INFO] Initiating bootcall Instance Manager..')
         aws.instancemanager.instancemanager.start_instance()
     else:
-        if args[0] == 'instance_manager':
+        if args[1] == 'instance_manager':
             print('[INFO] Initiating bootcall Instance Manager..')
             aws.instancemanager.instancemanager.start_instance()
-        elif args[0] == 'node_manager':
+        elif args[1] == 'node_manager':
             print('[INFO] Initiating bootcall Node Manager..')
             aws.nodemanager.nodemanager.start_instance()
-        elif args[0] == 'worker' or args[0] == 'node_worker':
+        elif args[1] == 'worker' or args[0] == 'node_worker':
             print('[INFO] Initiating bootcall Worker..')
             aws.nodeworker.nodeworker.start_instance()
-        elif args[0] == 'resource_manager':
+        elif args[1] == 'resource_manager':
             print('[INFO] Initiating bootcall Resource Manager..')
             aws.resourcemanager.resourcemanager.start_instance()
+        else:
+            print('[ERROR] Unknown argument passed to program {}\n'
+                  'Expected "instance_manager", "node_manager", "worker", or '
+                  '"resource_manager"'.format(args[1]))
     print('Program terminated')
 
 
