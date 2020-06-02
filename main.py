76c507fb644d63b81945df5c4c5cd3e7efd86f2a
Frenk <superdeboeren@hotmail.com>
2020-06-02 10:43:23 +0200

    Indication of main function function calls

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ import aws
 
 def main():
     args = str(sys.argv)
+    print(args)
     if not len(args):
+        print('[INFO] Initiating bootcall Instance Manager..')
         aws.instancemanager.instancemanager.start_instance()
     else:
         if args[0] == 'instance_manager':
+            print('[INFO] Initiating bootcall Instance Manager..')
             aws.instancemanager.instancemanager.start_instance()
         elif args[0] == 'node_manager':
+            print('[INFO] Initiating bootcall Node Manager..')
             aws.nodemanager.nodemanager.start_instance()
         elif args[0] == 'worker' or args[0] == 'node_worker':
+            print('[INFO] Initiating bootcall Worker..')
             aws.nodeworker.nodeworker.start_instance()
         elif args[0] == 'resource_manager':
+            print('[INFO] Initiating bootcall Resource Manager..')
             aws.resourcemanager.resourcemanager.start_instance()
     print('Program terminated')
 
