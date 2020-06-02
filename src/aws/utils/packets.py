a8983dd76f73b5b05bec88c8829a55b126059dc9
Frenk <superdeboeren@hotmail.com>
2020-06-02 14:44:55 +0200

    Made parameters of HeartBeatPackage public

--- src/aws/utils/packets.py
+++ src/aws/utils/packets.py
@@ -4,9 +4,9 @@ import time as timepackage
 class HeartBeatPacket(dict):
 
     def __init__(self, state, cpu_usage, mem_usage, time=None):
-        self._time = time if time else timepackage.time()
-        self._state = state
-        self._cpu_usage = cpu_usage
-        self._mem_usage = mem_usage
-        super().__init__(time=self._time, state=str(self._state), cpu_usage=self._cpu_usage,
-                         mem_usage=self._mem_usage)
+        self.time = time if time else timepackage.time()
+        self.state = state
+        self.cpu_usage = cpu_usage
+        self.mem_usage = mem_usage
+        super().__init__(time=self.time, state=str(self.state), cpu_usage=self.cpu_usage,
+                         mem_usage=self.mem_usage)
