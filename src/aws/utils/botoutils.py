84549f5fcf78167df67a4d4f54b1c3099fb72fdd
Frenk <superdeboeren@hotmail.com>
2020-06-03 23:17:46 +0200

    Temporarily removed BotoInstance

--- src/aws/utils/botoutils.py
+++ src/aws/utils/botoutils.py
@@ -42,46 +42,46 @@ class BotoInstanceReader:
         return False
 
 
-class BotoInstance:
-
-    @staticmethod
-    def instance(content):
-        return BotoInstance(**content)
-
-    def __init__(self, InstanceId, PublicDnsName, State, Tags, **kwargs):
-        self.instance_id = InstanceId
-        self.dns = PublicDnsName
-        self.state = State['Name']
-        self.tags = Tags
-        self.name = ''
-        for tag in self.tags:
-            if tag['Key'] == 'Name':
-                self.name = self.map_name(tag['Value'])
-                break
-
-    def __str__(self) -> str:
-        name = self.name if self.name != '' else 'BotoInstance'
-        return "{}[instance_id: {}, dns: {}, state: {}, tags: {}]".format(
-            name, self.instance_id, self.dns, self.state, self.tags)
-
-    def __repr__(self):
-        return self.__str__()
-
-    @staticmethod
-    def map_name(name: str):
-        return name.lower().replace('_', ' ')
-
-    def is_running(self) -> bool:
-        return self.state == 'running'
-
-    def is_worker(self) -> bool:
-        return self.name == 'Worker'
-
-    def is_instance_manger(self) -> bool:
-        return self.name == 'Instance Manager'
-
-    def is_node_manager(self) -> bool:
-        return self.name == 'Node Manager'
-
-    def is_resource_manager(self) -> bool:
-        return self.name == 'Resource Manager'
+# class BotoInstance:
+#
+#     @staticmethod
+#     def instance(content):
+#         return BotoInstance(**content)
+#
+#     def __init__(self, InstanceId, PublicDnsName, State, Tags, **kwargs):
+#         self.instance_id = InstanceId
+#         self.dns = PublicDnsName
+#         self.state = State['Name']
+#         self.tags = Tags
+#         self.name = ''
+#         for tag in self.tags:
+#             if tag['Key'] == 'Name':
+#                 self.name = self.map_name(tag['Value'])
+#                 break
+#
+#     def __str__(self) -> str:
+#         name = self.name if self.name != '' else 'BotoInstance'
+#         return "{}[instance_id: {}, dns: {}, state: {}, tags: {}]".format(
+#             name, self.instance_id, self.dns, self.state, self.tags)
+#
+#     def __repr__(self):
+#         return self.__str__()
+#
+#     @staticmethod
+#     def map_name(name: str):
+#         return name.lower().replace('_', ' ')
+#
+#     def is_running(self) -> bool:
+#         return self.state == 'running'
+#
+#     def is_worker(self) -> bool:
+#         return self.name == 'Worker'
+#
+#     def is_instance_manger(self) -> bool:
+#         return self.name == 'Instance Manager'
+#
+#     def is_node_manager(self) -> bool:
+#         return self.name == 'Node Manager'
+#
+#     def is_resource_manager(self) -> bool:
+#         return self.name == 'Resource Manager'
