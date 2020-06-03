f58d48f9e9f78c9cb8e63b0d046675f0d4ed77b1
Frenk <superdeboeren@hotmail.com>
2020-06-03 23:22:45 +0200

    BotoInstanceReader call

--- src/aws/instancemanager/instancemanager.py
+++ src/aws/instancemanager/instancemanager.py
@@ -169,8 +169,7 @@ class NodeScheduler:
     def run(self, lock):
         try:
             while True:
-                print(self.instance_id)
-                # boto_response = BotoInstanceReader().read(self.instance_id)
+                boto_response = BotoInstanceReader().read(self.instance_id)
                 with lock:
                 #    self.instances.update_all(boto_response=boto_response)
                    print(self.instances)
