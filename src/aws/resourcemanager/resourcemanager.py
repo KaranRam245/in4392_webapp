d2e5e1798cffe934bd801ddcbf35a8bddc4506a4
Sander Waij <S.Waij@student.tudelft.nl>
2020-06-03 12:55:31 +0200

    Added create_bucket

--- src/aws/resourcemanager/resourcemanager.py
+++ src/aws/resourcemanager/resourcemanager.py
@@ -3,16 +3,37 @@ Module for the Resource Manager.
 """
 from aws.utils.monitor import Listener
 from S3Connector import S3Connector
+import uuid
 
 
 class ResourceManagerCore:
 
     def __init__(self):
-        pass
+        self.S3 = S3Connector()
 
     def run(self):
-        raise NotImplementedError()
+        create_bucket()
+        print(self.S3.get_bucket_list())
 
+    if __name__ == "__main__":
+        ResourceManagerCore().run()
+
+    def create_bucket(self):
+        """
+        Method called to create a bucket.
+        """
+        bucket_name = str(uuid.uuid4())
+        bucket_region = self.S3.aws_region
+        if bucket_region == 'us-east-1':
+            bucket_response = self.S3.create_bucket(
+                Bucket=bucket_name)
+        else:
+            bucket_response = self.S3.create_bucket(
+                Bucket=bucket_name,
+                BucketConfiguration={
+                    'LocationConstraint': bucket_region})
+        print(bucket_name, bucket_region)
+        return bucket_name, bucket_response
 
 class ResourceMonitor(Listener):
 
@@ -27,15 +48,3 @@ class ResourceMonitor(Listener):
         """
         raise NotImplementedError("The class is a listener but has not implemented the event "
                                   "method.")
-
-class S3Connector(self, key_id,access_key,aws_reg,conf):
-
-    def __init__(self):
-        self.S3 = S3Connector(
-            # aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-            # aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-            # aws_region=os.environ['AWS_REGION']
-        )
-
-    def run(self):
-        raise NotImplementedError()
