4e751920badf62117c5dbf4b17a3e080bc822cbe
Frenk <superdeboeren@hotmail.com>
2020-06-03 23:56:33 +0200

    Use the boto_reader

--- src/aws/instancemanager/instancemanager.py
+++ src/aws/instancemanager/instancemanager.py
@@ -174,7 +174,7 @@ class NodeScheduler:
         print("Reader created..")
         try:
             while True:
-                # boto_response = boto_reader.read(self.instance_id)
+                boto_response = boto_reader.read(self.instance_id)
                 # with lock:
                 #    self.instances.update_all(boto_response=boto_response)
                 print(self.instances)
