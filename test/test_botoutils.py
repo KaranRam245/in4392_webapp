d0ee5eb34a4190d977a9678487eed06efbd15ca3
Frenk <superdeboeren@hotmail.com>
2020-06-05 13:28:30 +0200

    Updated tests for BotoIntanceReader

--- test/test_botoutils.py
+++ test/test_botoutils.py
@@ -8,14 +8,14 @@ class TestBotoIntanceReader(unittest.TestCase):
         with open('test_files/boto_result.json') as file:
             boto_result = file.readlines()
             boto_result = ''.join(boto_result)
-            boto_instances = BotoInstanceReader.read(boto_result, 'i-0c785590248a26fa6')
+            boto_instances = BotoInstanceReader().read(boto_result, 'i-0c785590248a26fa6')
             print('No filter: {}'.format(boto_instances))
 
     def test_simple_run_filter(self):
         with open('test_files/boto_result.json') as file:
             boto_result = file.readlines()
             boto_result = ''.join(boto_result)
-            boto_instances = BotoInstanceReader.read(boto_result, 'i-0c785590248a26fa6',
+            boto_instances = BotoInstanceReader().read(boto_result, 'i-0c785590248a26fa6',
                                                      filters=['is_node_manager'])
             print('Node managers: {}'.format(boto_instances))
 
@@ -23,7 +23,7 @@ class TestBotoIntanceReader(unittest.TestCase):
         with open('test_files/boto_result.json') as file:
             boto_result = file.readlines()
             boto_result = ''.join(boto_result)
-            boto_instances = BotoInstanceReader.read(boto_result, 'i-0c785590248a26fa6',
+            boto_instances = BotoInstanceReader().read(boto_result, 'i-0c785590248a26fa6',
                                                      filters=['is_node_manager', 'is_running'])
             print('Started: {}'.format(boto_instances))
 
@@ -31,7 +31,7 @@ class TestBotoIntanceReader(unittest.TestCase):
         with open('test_files/boto_result.json') as file:
             boto_result = file.readlines()
             boto_result = ''.join(boto_result)
-            boto_instances = BotoInstanceReader.read(boto_result, 'i-0c785590248a26fa6',
+            boto_instances = BotoInstanceReader().read(boto_result, 'i-0c785590248a26fa6',
                                                      filters=['is_node_manager',
                                                               ('is_running', False)])
             print('Not Started: {}'.format(boto_instances))
