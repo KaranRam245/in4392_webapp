1cf5f9e981c17cec34347beea7f2b7dc9e2284ca
Frenk <superdeboeren@hotmail.com>
2020-06-05 18:58:11 +0200

    TaskPool server and client

--- src/aws/nodemanager/nodemanager.py
+++ src/aws/nodemanager/nodemanager.py
@@ -6,7 +6,7 @@ import asyncio
 import aws.utils.connection as con
 from aws.utils.connection import MultiConnectionClient
 from aws.utils.monitor import Listener, Observable
-from aws.utils.packets import HeartBeatPacket
+from aws.utils.packets import HeartBeatPacket, CommandPacket
 from aws.utils.state import InstanceState
 
 
@@ -43,17 +43,28 @@ class TaskPool(Observable):
             message=HeartBeatPacket(state=self._instance_state, instance_type='node_manager'))
 
 
-class TaskPoolMonitor(Listener, MultiConnectionClient):
-
-    def __init__(self, taskpool, host, port):
-        self._tp = taskpool
-        super().__init__(host, port)
-
-    def event(self, message):
-        self.send_message(message)
+class TaskPoolClientWrapper(con.MultiConnectionClient):
 
     def process_command(self, command):
         print("Need help with command: {}".format(command))
+
+
+class TaskPoolServerWrapper(con.MultiConnectionServer):
+
+    def process_command(self, command: CommandPacket):
+        print("A client send me a command: {}".format(command))
+
+
+class TaskPoolMonitor(Listener):
+
+    def __init__(self, taskpool, client, server):
+        self._tp = taskpool
+        self.client = client
+        self.server = server
+        super().__init__()
+
+    def event(self, message):
+        self.client.send_message(message)
 
 
 def start_instance(host, port=con.PORT):
@@ -61,11 +72,13 @@ def start_instance(host, port=con.PORT):
     Function to start the Node Scheduler, which is the heart of the Instance Manager.
     """
     taskpool = TaskPool()
-    monitor = TaskPoolMonitor(taskpool, host, port)
+    taskpool_client = TaskPoolClientWrapper(host, port)
+    taskpool_server = TaskPoolServerWrapper(host, port)
+    monitor = TaskPoolMonitor(taskpool, taskpool_client, taskpool_server)
     taskpool.add_listener(monitor)
 
     loop = asyncio.get_event_loop()
-    procs = asyncio.wait([taskpool.run(), monitor.run()])
+    procs = asyncio.wait([taskpool.run(), monitor.client.run(), monitor.server.run()])
     loop.run_until_complete(procs)
     loop.close()
 
