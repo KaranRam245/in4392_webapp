78df8b0d9acffc38518461c69670b5588e35070e
Frenk <superdeboeren@hotmail.com>
2020-06-06 12:15:39 +0200

    Node worker task queue
    Changes:
    - Worker task queue
    - Task commands are added to the queue and popped when no task is running
    - Heartbeat and run in worker are seperate functions.
    - Introduction for unimplemented kill and stop commands.

--- src/aws/nodeworker/nodeworker.py
+++ src/aws/nodeworker/nodeworker.py
@@ -2,8 +2,8 @@
 Module for the Node Worker.
 """
 import asyncio
-import aws.utils.connection as con
 
+import aws.utils.connection as con
 from aws.utils.connection import MultiConnectionClient
 from aws.utils.monitor import Observable, Listener
 from aws.utils.packets import CommandPacket, HeartBeatPacket
@@ -15,12 +15,14 @@ class WorkerCore(Observable):
     The WorkerCore accepts the task from the Node Manager.
     """
 
-    def __init__(self):
+    def __init__(self, task_queue):
         super().__init__()
+        self._task_queue = task_queue
+        self.current_task = None
         self._instance_state = InstanceState(InstanceState.RUNNING)
         self._program_state = ProgramState(ProgramState.PENDING)
 
-    async def run(self):
+    async def heartbeat(self):
         """
         Start function for the WorkerCore.
         """
@@ -28,6 +30,20 @@ class WorkerCore(Observable):
             while True:
                 self.generate_heartbeat()
                 await asyncio.sleep(15)
+        except KeyboardInterrupt:
+            pass
+
+    async def run(self):
+        """
+        Start function for the WorkerCore.
+        """
+        try:
+            while True:
+                if not self.current_task and self._task_queue:
+                    self.current_task = self._task_queue.pop(0)
+                    await asyncio.sleep(2)  # TODO: replace with actual processing here!
+                    self.current_task = None
+                await asyncio.sleep(1)
         except KeyboardInterrupt:
             pass
 
@@ -39,7 +55,8 @@ class WorkerCore(Observable):
 
 class WorkerMonitor(Listener, MultiConnectionClient):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, task_queue):
+        self._task_queue = task_queue
         super().__init__(host, port)
 
     def event(self, message):
@@ -52,20 +69,22 @@ class WorkerMonitor(Listener, MultiConnectionClient):
 
     def process_command(self, command: CommandPacket):
         if command['command'] == 'stop':
-            raise NotImplementedError("Client has not yet implemented process_command.")
-        elif command['command'] == 'task':
-            raise NotImplementedError("Client has not yet implemented process_command.")
-        else:
-            print('Received unknown command: {}'.format(command['command']))
+            raise NotImplementedError("Client has not yet implemented [stop].")
+        if command['command'] == '`kill`':
+            raise NotImplementedError("Client has not yet implemented [kill].")
+        if command['command'] == 'task':
+            self._task_queue.put(command)
+        print('Received unknown command: {}'.format(command['command']))
 
 
 def start_instance(host, port=con.PORT):
-    worker_core = WorkerCore()
-    monitor = WorkerMonitor(host, port)
+    task_queue = []
+    worker_core = WorkerCore(task_queue)
+    monitor = WorkerMonitor(host, port, task_queue)
     worker_core.add_listener(monitor)
 
     loop = asyncio.get_event_loop()
-    procs = asyncio.wait([worker_core.run(), monitor.run()])
+    procs = asyncio.wait([worker_core.run(), worker_core.heartbeat(), monitor.run()])
     try:
         tasks = loop.run_until_complete(procs)
         tasks.close()
