ea3f5bab8ef35e766a776cb0f41e6d02502ec797
Sander Waij <S.Waij@student.tudelft.nl>
2020-06-15 22:16:17 +0200

    Removed last signal print line

--- src/aws/instancemanager/instancemanager.py
+++ src/aws/instancemanager/instancemanager.py
@@ -138,14 +138,6 @@ class Instances:
     def start_signal_timedout(self, instance_id):
         signal_time = self._start_signal.get(instance_id, None)
         current_time = time()
-        diff = current_time - signal_time if signal_time else 999
-        log_info('Instance: {}, Last signal_time:{}, time:{}, subtract:{}, {}. sent: {}'.format(
-            instance_id,
-            signal_time,
-            current_time,
-            diff,
-            diff >= config.START_SIGNAL_TIMEOUT,
-            self._start_signal))
         if not signal_time:
             return True
         return (current_time - signal_time) >= config.START_SIGNAL_TIMEOUT
